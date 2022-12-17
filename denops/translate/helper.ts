73f6a30db3839f0c97907d243241c0ebaaf313cc
kamecha <masakikameyama81@gmail.com>
2022-12-17 19:39:15 +0900

    lspのフォーマットをした

--- denops/translate/helper.ts
+++ denops/translate/helper.ts
@@ -1,141 +1,141 @@
 import { Denops, ensureString, path, vars, xdg } from "./deps.ts";
 
 export const deeplAuthokeyPath = path.join(
-  xdg.config(),
-  "denops_translate",
-  "deepl_authkey",
+	xdg.config(),
+	"denops_translate",
+	"deepl_authkey",
 );
 
 export const onece = <A extends unknown, R extends Promise<unknown>>(
-  f: (arg?: A) => R,
+	f: (arg?: A) => R,
 ) => {
-  let v: R | undefined;
-  return (arg?: A): R => {
-    return v || (v = f(arg));
-  };
+	let v: R | undefined;
+	return (arg?: A): R => {
+		return v || (v = f(arg));
+	};
 };
 
 export async function readDeepLAuthkey(): Promise<string> {
-  try {
-    const text = await Deno.readTextFile(deeplAuthokeyPath);
-    return text.trim();
-  } catch (e) {
-    throw new Error(
-      `Cannot read DeepL's authkey from ${deeplAuthokeyPath}: ${e.message}`,
-    );
-  }
+	try {
+		const text = await Deno.readTextFile(deeplAuthokeyPath);
+		return text.trim();
+	} catch (e) {
+		throw new Error(
+			`Cannot read DeepL's authkey from ${deeplAuthokeyPath}: ${e.message}`,
+		);
+	}
 }
 
 export const getDeepLAuthKey = onece(readDeepLAuthkey);
 
 export type Option = {
-  endpoint: string;
-  isDeepL: boolean;
-  authKey?: string;
-  source: string;
-  target: string;
-  text: string;
+	endpoint: string;
+	isDeepL: boolean;
+	authKey?: string;
+	source: string;
+	target: string;
+	text: string;
 };
 
 export type Position = {
-  lnum: number;
-  col: number;
+	lnum: number;
+	col: number;
 };
 
 export async function buildOption(
-  denops: Denops,
-  bang: boolean,
-  startLnum: number,
-  startCol: number,
-  endLnum: number,
-  endCol: number,
-  arg: string,
+	denops: Denops,
+	bang: boolean,
+	startLnum: number,
+	startCol: number,
+	endLnum: number,
+	endCol: number,
+	arg: string,
 ): Promise<Option> {
-  const parts: string[] = [];
+	const parts: string[] = [];
 
-  let i = 0;
-  const max = arg.length;
-  let word = "";
-  let skipWhitespace = false;
+	let i = 0;
+	const max = arg.length;
+	let word = "";
+	let skipWhitespace = false;
 
-  while (i < max) {
-    const char = arg[i];
-    if (char === '"') {
-      skipWhitespace = !skipWhitespace;
-      i++;
-      if (i < max && arg[i] === " ") {
-        word += arg[i];
-        i++;
-      }
-      continue;
-    }
-    if (!skipWhitespace && char === " ") {
-      parts.push(word);
-      word = "";
-    } else {
-      word += char;
-    }
-    i++;
-  }
+	while (i < max) {
+		const char = arg[i];
+		if (char === '"') {
+			skipWhitespace = !skipWhitespace;
+			i++;
+			if (i < max && arg[i] === " ") {
+				word += arg[i];
+				i++;
+			}
+			continue;
+		}
+		if (!skipWhitespace && char === " ") {
+			parts.push(word);
+			word = "";
+		} else {
+			word += char;
+		}
+		i++;
+	}
 
-  if (word !== "") {
-    parts.push(word);
-  }
+	if (word !== "") {
+		parts.push(word);
+	}
 
-  const message: string[] = [];
-  const startPos: Position = { lnum: startLnum, col: startCol };
-  const endPos: Position = { lnum: endLnum, col: endCol };
+	const message: string[] = [];
+	const startPos: Position = { lnum: startLnum, col: startCol };
+	const endPos: Position = { lnum: endLnum, col: endCol };
 
-  switch (parts.length) {
-    case 1:
-      ensureString(parts[0]);
-      message.push(parts[0]);
-      break;
-    case 2:
-    case 0:
-      for (const [index, m] of ( (await denops.call("getline", startPos.lnum, endPos.lnum)) as string[] ).entries()) {
-	let editedMessage = m;
-	if (startPos.lnum === endPos.lnum) {
-		editedMessage = editedMessage.substring(startPos.col, endPos.col);
-	} else {
-		if (index === 0) {
-			editedMessage = editedMessage.substring(startPos.col);
-		} else if (startPos.lnum + index === endPos.lnum) {
-			editedMessage = editedMessage.substring(0, endPos.col);
+	switch (parts.length) {
+		case 1:
+			ensureString(parts[0]);
+			message.push(parts[0]);
+			break;
+		case 2:
+		case 0:
+			for (const [index, m] of ((await denops.call("getline", startPos.lnum, endPos.lnum)) as string[]).entries()) {
+				let editedMessage = m;
+				if (startPos.lnum === endPos.lnum) {
+					editedMessage = editedMessage.substring(startPos.col, endPos.col);
+				} else {
+					if (index === 0) {
+						editedMessage = editedMessage.substring(startPos.col);
+					} else if (startPos.lnum + index === endPos.lnum) {
+						editedMessage = editedMessage.substring(0, endPos.col);
+					}
+				}
+				message.push(editedMessage);
+			}
+			break;
+		default: {
+			message.push(parts[2]);
 		}
 	}
-        message.push(editedMessage);
-      }
-      break;
-    default: {
-      message.push(parts[2]);
-    }
-  }
 
-  const source = await vars.g.get<string>(denops, "translate_source", "en");
-  const target = await vars.g.get<string>(denops, "translate_target", "ja");
-  const endpoint = await vars.g.get<string>(denops, "translate_endpoint", "");
+	const source = await vars.g.get<string>(denops, "translate_source", "en");
+	const target = await vars.g.get<string>(denops, "translate_target", "ja");
+	const endpoint = await vars.g.get<string>(denops, "translate_endpoint", "");
 
-  const opt = {
-    endpoint: endpoint,
-    isDeepL: endpoint.includes("deepl.com"),
-    source: source,
-    target: target,
-    text: message.join("\n"),
-  } as Option;
+	const opt = {
+		endpoint: endpoint,
+		isDeepL: endpoint.includes("deepl.com"),
+		source: source,
+		target: target,
+		text: message.join("\n"),
+	} as Option;
 
-  if (parts.length >= 2) {
-    opt.source = parts[0];
-    opt.target = parts[1];
-  }
+	if (parts.length >= 2) {
+		opt.source = parts[0];
+		opt.target = parts[1];
+	}
 
-  if (bang) {
-    [opt.source, opt.target] = [opt.target, opt.source];
-  }
+	if (bang) {
+		[opt.source, opt.target] = [opt.target, opt.source];
+	}
 
-  if (opt.isDeepL) {
-    opt.authKey = await getDeepLAuthKey();
-  }
+	if (opt.isDeepL) {
+		opt.authKey = await getDeepLAuthKey();
+	}
 
-  return opt;
+	return opt;
 }
